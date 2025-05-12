5871d09d7fb20e06d34e0a8514516459af280598
matthewvaccaro <matthew.tylor.vaccaro@gmail.com>
2025-05-12 11:25:30 -0500

    setup up for technical

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { NavLink } from "react-router";
+import { API } from "./api/axios";
 
 function App() {
   const [count, setCount] = useState(0);
+  API.get("/")
+    .then((res) => console.log(res.data))
+    .catch((e) => console.error(e));
 
   return (
     <>
